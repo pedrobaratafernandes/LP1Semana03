c28fc512898fdc87188b609643ab7f4218faf58d
Pedro Fernandes <pedro.barata.fernandes@gmail.com>
2026-03-04 00:08:18 +0000

    Exercicio 2

---
+++ ChangeString/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ChangeString
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("String: ");
+            string frase = Console.ReadLine();
+
+            Console.Write($"Caracter: ");
+            char c = char.Parse(Console.ReadLine());
+
+            foreach (char f in frase)
+            {
+                if (f != c)
+                {
+                    Console.Write(f);
+                }
+                if (f == c)
+                {
+                    Console.Write("x");
+                }
+            }
+        }
+    }
+}
