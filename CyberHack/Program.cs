18e27afa71f9c85cd779c3c27fc99f88e22a194c
Pedro Fernandes <pedro.barata.fernandes@gmail.com>
2026-03-05 16:26:48 +0000

    4

--- CyberHack/Program.cs
+++ CyberHack/Program.cs
@@ -6,7 +6,87 @@ namespace CyberHack
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP!");
+            HackTool hackTool = 0;
+            SystemType systemType = 0;
+
+            string system = Console.ReadLine();
+
+            string hacktools = Console.ReadLine();
+
+            if (system == "CorporateServer")
+            {
+                systemType |= SystemType.CorporateServer;
+            }
+            else if (system == "BankDatabase")
+            {
+                systemType |= SystemType.BankDatabase;
+            }
+            else if (system == "SmartCityCore")
+            {
+                systemType |= SystemType.SmartCityCore;
+            }
+            else if (system == "MilitaryAI")
+            {
+                systemType |= SystemType.MilitaryAI;
+            }
+
+
+
+            if (hacktools == "Phishing")
+            {
+                hackTool |= HackTool.Phishing;
+
+
+            }
+            else if (hacktools == "Backdoor")
+            {
+                hackTool |= HackTool.Backdoor;
+            }
+            else if (hacktools == "BruteForce")
+            {
+                hackTool |= HackTool.BruteForce;
+            }
+
+            else if (hacktools == "ZeroDay")
+            {
+                hackTool |= HackTool.ZeroDay;
+            }
+            else if (hacktools == "AIOverride")
+            {
+                hackTool |= HackTool.AIOverride;
+            }
+            else if (hacktools == "QuantumExploit")
+            {
+                hackTool |= HackTool.QuantumExploit;
+            }
+
+
+            if ((systemType & (SystemType.CorporateServer)) == (SystemType.CorporateServer))
+            {
+
+                if ((hackTool & (HackTool.Phishing & HackTool.Backdoor)) == (HackTool.ZeroDay & HackTool.BruteForce))
+                {
+                    Console.WriteLine("System Hacked!");
+                }
+                else
+                {
+                    Console.WriteLine("System Secure");
+                }
+            } else if ((systemType & (SystemType.BankDatabase)) == (SystemType.BankDatabase))
+            {
+
+                if ((hackTool & (HackTool.ZeroDay & HackTool.Backdoor)) == (HackTool.ZeroDay & HackTool.BruteForce))
+                {
+                    Console.WriteLine("System Hacked!");
+                }
+                else
+                {
+                    Console.WriteLine("System Secure");
+                }
+            }
+
+
+
         }
     }
 }
