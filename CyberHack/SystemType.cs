5e740c9299f72d2805bfeb20ec5f9ee036efffff
Pedro Fernandes <pedro.barata.fernandes@gmail.com>
2026-03-05 15:57:31 +0000

    2

---
+++ CyberHack/SystemType.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace CyberHack
+{
+    [Flags]
+    public enum SystemType
+    {
+        CorporateServer,
+        BankDatabase,
+        SmartCityCore,
+        MilitaryAI
+    }
+}
